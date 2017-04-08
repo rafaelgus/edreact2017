42f09a90b1788487566fefdacae8664b5bd00cde
Jonathan MirCha <jonmircha@gmail.com>
2017-04-07 19:39:26 -0500

    Clase 2 Curso React desde cero de EDteam

---
+++ 00-configuracion/la-fea/src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyAwesomeApp from './MyAwesomeApp'
+import './app.css'
+
+ReactDOM.render(
+  <MyAwesomeApp name="React" />,
+  document.getElementById('app')
+)
